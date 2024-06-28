88988ae24fdcb103776a43c4f8313706d70b2289
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2024-06-28 12:46:07 -0400

    add logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ConfigService } from './config/config.service';
 import configuration from './config/configuration';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from '@algoan/nestjs-logging-interceptor';
 
 @Module({
   imports: [
@@ -13,6 +15,10 @@ import configuration from './config/configuration';
     }),
   ],
   controllers: [AppController],
-  providers: [AppService, ConfigService],
+  providers: [
+    AppService,
+    ConfigService,
+    { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
+  ],
 })
 export class AppModule {}
